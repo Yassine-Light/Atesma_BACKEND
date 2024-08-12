dee6ee7b30fe4daa5a1a7fe96eaf38c6a3c714a2
Mohamed Hajjouji <mohamedhajjouji66@gmail.com>
2024-08-12 19:49:21 +0100

    Login Page added

---
+++ login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login page</title>
+</head>
+<body>
+    
+<main class="account-main">
+        <div class="container">
+            <h1>Login</h1>
+            <div>
+                <label for="">Email:</label>
+                <input type="email" name="" id="">
+            </div>
+            <div>
+                <label for="">Password:</label>
+                <input type="email" name="" id="">
+            </div>
+            <button class="login">Login</button>
+        </div>
+        
+    </main>
+    
+</body>
+</html>
